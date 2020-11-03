b6ec70e052b0da21ede1850d1cfc533662e2f970
Nikola <nikolanenchevnikolov@gmail.com>
2020-11-03 11:43:58 +0200

    added state to eventform

--- app/javascript/packs/EventForm.js
+++ app/javascript/packs/EventForm.js
@@ -1,14 +1,24 @@
 import React from 'react';
 
-class EventForm extends React.Component { 
+class EventForm extends React.Component {
+
+  constructor(props) { 
+    super(props)
+    this.state = {
+      title: '',
+      start_datetime: '',
+      location: ''
+    }
+  }
+
   render() { 
     return (
       <div>
         <h4>Create event: </h4>
         <form>
-          <input type='text' name='title' placeholder='Title' />
-          <input type='text' name='start_datetime' placeholder='Date' />
-          <input type='text' name='location' placeholder='Location' />
+          <input type='text' name='title' placeholder='Title' value={this.state.title} />
+          <input type='text' name='start_datetime' placeholder='Date' value={this.state.start_datetime} />
+          <input type='text' name='location' placeholder='Location' value={this.state.location} />
           <button type='submit'>Create Event</button>
         </form>
       </div>
