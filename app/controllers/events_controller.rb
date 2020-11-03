ba64f9e1f3b713d8b9508fca6c756552650cdeed
Nikola <nikolanenchevnikolov@gmail.com>
2020-11-03 17:58:04 +0200

    basic crud for events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,14 @@
 class EventsController < ApplicationController
+  before_action :set_event, only: %i[show update destroy]
+
   def index
     @events = Event.order('start_datetime ASC')
     render json: @events
+  end
+
+  def show
+    render json: @event.as_json(except: :user_id, include: { user: { only: [:name] } })
+      .merge(currentUserCanEdit: @event.user.email == request.headers['uid'])
   end
 
   def create
@@ -14,7 +21,29 @@ class EventsController < ApplicationController
     end
   end
 
+  def update
+    @event = current_user.events.find(params[:id])
+
+    if @event.update(event_params)
+      render json: @event
+    else
+      render json: @event.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @event.destroy
+      head :no_content, status: :ok
+    else
+      render json: @event.errors, status: :unprocessable_entity
+    end
+  end
+
   private
+
+  def set_event
+    @event = Event.find(params[:id])
+  end
 
   def event_params
     params.require(:event).permit(:title, :start_datetime, :location)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_03_133039) do
+ActiveRecord::Schema.define(version: 2020_11_03_152333) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2020_11_03_133039) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
+    t.text "description"
+    t.string "image_url"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
