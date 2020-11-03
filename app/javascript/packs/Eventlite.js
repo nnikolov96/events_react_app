6b013e5aa8e093188b68a6e5bbf2dd6791560dfe
Nikola <nikolanenchevnikolov@gmail.com>
2020-11-03 13:48:55 +0200

    errors on every field in event object

--- app/javascript/packs/Eventlite.js
+++ app/javascript/packs/Eventlite.js
@@ -10,9 +10,9 @@ class Eventlite extends React.Component {
     super(props);
     this.state = {
       events: this.props.events,
-      title: "",
-      start_datetime: "",
-      location: "",
+      title: { value: '', valid: false },
+      start_datetime: { value: '', valid: false },
+      location: { value: '', valid: false },
       formValid: false,
       formErrors: {}
     };
@@ -22,7 +22,7 @@ class Eventlite extends React.Component {
     e.preventDefault();
     const name = e.target.name;
     const newState = {};
-    newState[name] = e.target.value;
+    newState[name] = {...this.state[name], value: e.target.value};
     this.setState(newState, this.validateForm);
   };
 
@@ -30,32 +30,39 @@ class Eventlite extends React.Component {
     let formErrors = {}
     let formValid = true
 
-    if(this.state.title.length <= 2) {
+    if(this.state.title.value.length <= 2) {
       formErrors.title = ["is too short (minimum is 3 characters)"];
       formValid = false;
     }
 
-    if (this.state.location.length === 0) {
+    if (this.state.location.value.length === 0) {
       formErrors.location = ["can't be blank"];
       formValid = false;
     }
-    if (this.state.start_datetime.length === 0) {
+    if (this.state.start_datetime.value.length === 0) {
       formErrors.start_datetime = ["can't be blank"];
       formValid = false;
-    } else if (Date.parse(this.state.start_datetime) <= Date.now()) {
+    } else if (Date.parse(this.state.start_datetime.value) <= Date.now()) {
       formErrors.start_datetime = ["can't be in the past"];
       formValid = false;
     }
-    
+
     this.setState({ formValid: formValid, formErrors: formErrors });
   }
 
   handleSubmit = (e) => {
+    let newEvent = {
+      title: this.state.title.value,
+      start_datetime: this.state.start_datetime.value,
+      location: this.state.location.value,
+    };
     e.preventDefault();
     axios({
       method: "POST",
       url: "/events",
-      data: { event: this.state },
+      data: { 
+        event: newEvent
+      },
       headers: {
         "X-CSRF-Token": document.querySelector("meta[name=csrf-token]").content,
       },
@@ -87,9 +94,9 @@ class Eventlite extends React.Component {
         <EventForm
           handleSubmit={this.handleSubmit}
           handleInput={this.handleInput}
-          title={this.state.title}
-          start_datetime={this.state.start_datetime}
-          location={this.state.location}
+          title={this.state.title.value}
+          start_datetime={this.state.start_datetime.value}
+          location={this.state.location.value}
           formValid={this.state.formValid}
         />
         <EventList events={this.state.events} />
