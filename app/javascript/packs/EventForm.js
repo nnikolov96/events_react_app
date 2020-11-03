5d0b85655ffd6900c85a987aa0479c09694f27a0
Nikola <nikolanenchevnikolov@gmail.com>
2020-11-03 12:14:03 +0200

    lifted state to parent component

--- app/javascript/packs/EventForm.js
+++ app/javascript/packs/EventForm.js
@@ -1,54 +1,14 @@
-import axios from 'axios';
 import React from 'react';
 
-class EventForm extends React.Component {
-
-  constructor(props) { 
-    super(props)
-    this.state = {
-      title: '',
-      start_datetime: '',
-      location: ''
-    }
-  }
-
-  handleInput = e => {
-    const name = e.target.name;
-    const newState = {};
-    newState[name] = e.target.value;
-    this.setState(newState)
-    e.preventDefault();
-  }
-
-  handleSubmit = e => { 
-    axios({
-      method: "POST",
-      url: "/events",
-      data: { event: this.state },
-      headers: {
-        "X-CSRF-Token": document.querySelector("meta[name=csrf-token]").content,
-      }
-    })
-    .then(response => {
-      this.props.handleNewEvent(response.data);
-    })
-    .catch(error => console.log(error));
-    e.preventDefault();
-  }
-
-  render() { 
-    return (
-      <div>
-        <h4>Create event: </h4>
-        <form onSubmit = { this.handleSubmit }>
-          <input type='text' name='title' placeholder='Title' value={this.state.title} onChange = { this.handleInput }/>
-          <input type='text' name='start_datetime' placeholder='Date' value={this.state.start_datetime} onChange={this.handleInput} />
-          <input type='text' name='location' placeholder='Location' value={this.state.location} onChange={this.handleInput} />
-          <button type='submit'>Create Event</button>
-        </form>
-      </div>
-    );
-  }
-}
-
+const EventForm = (props) => (
+  <div>
+    <h4>Create an Event:</h4>
+    <form onSubmit={props.handleSubmit}>
+      <input type="text" name="title" placeholder="Title" value={props.title} onChange={props.handleInput} />
+      <input type="text" name="start_datetime" placeholder="Date" value={props.start_datetime} onChange={props.handleInput} />
+      <input type="text" name="location" placeholder="Location" value={props.location} onChange={props.handleInput} />
+      <input type="submit" value="Create Event" />
+    </form>
+  </div>
+)
 export default EventForm;
--- app/javascript/packs/Eventlite.js
+++ app/javascript/packs/Eventlite.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import axios from 'axios'
 import EventList from './EventList'
 import EventForm from './EventForm'
 
@@ -8,19 +9,55 @@ class Eventlite extends React.Component {
     super(props);
     this.state = {
       events: this.props.events,
+      title: "",
+      start_datetime: "",
+      location: "",
     };
   }
 
-  addNewEvent = event => { 
-    const events = [event, ...this.state.events].sort(function (a, b) {
+  handleInput = (e) => {
+    e.preventDefault();
+    const name = e.target.name;
+    const newState = {};
+    newState[name] = e.target.value;
+    this.setState(newState);
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    axios({
+      method: "POST",
+      url: "/events",
+      data: { event: this.state },
+      headers: {
+        "X-CSRF-Token": document.querySelector("meta[name=csrf-token]").content,
+      },
+    })
+      .then((response) => {
+        this.addNewEvent(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  addNewEvent = (event) => {
+    const events = [...this.state.events, event].sort(function (a, b) {
       return new Date(a.start_datetime) - new Date(b.start_datetime);
     });
-    this.setState({ events: events })
-  }
+    this.setState({ events: events });
+  };
+
   render() {
     return (
       <div>
-        <EventForm handleNewEvent={this.addNewEvent} />
+        <EventForm
+          handleSubmit={this.handleSubmit}
+          handleInput={this.handleInput}
+          title={this.state.title}
+          start_datetime={this.state.start_datetime}
+          location={this.state.location}
+        />
         <EventList events={this.state.events} />
       </div>
     );
