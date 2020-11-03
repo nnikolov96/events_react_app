33518a26ec2df76e8f8497cfb0986dd21f95d061
Nikola <nikolanenchevnikolov@gmail.com>
2020-11-03 11:50:58 +0200

    create action for events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,20 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def create
+    @event = Event.new(event_params)
+
+    if @event.save
+      render json: @event
+    else
+      render json: @event.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :start_datetime, :location)
+  end
 end
