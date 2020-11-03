09c649359338e342ea432f936a1a9c552eaee1b4
Nikola <nikolanenchevnikolov@gmail.com>
2020-11-03 11:34:19 +0200

    events are now populated from db

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,16 +1,5 @@
 class EventsController < ApplicationController
   def index
-    @events = [
-      {
-        title: "London Retail Expo",
-        datetime: "Monday 14 Oct, 2019",
-        location: "London Excel Centre"
-      },
-      {
-        title: "Enterprise Sales Training Workshop",
-        datetime: "Tuesday 15 Oct, 2019",
-        location: "Expert Sales Company Headquarters"
-      }
-    ]
+    @events = Event.all
   end
 end
